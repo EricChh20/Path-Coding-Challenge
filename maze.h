c84fdeeab98c03ca29f0a8f94fc8b13957a8e323
Eric Chheang <et.chheang@gmail.com>
2022-03-23 20:46:29 -0400

    User Story 3 functionality and UT passing

--- maze.h
+++ maze.h
@@ -3,13 +3,15 @@
 
 #include <iostream>
 #include <stdio.h> 
+#include <stack>
+#include <queue> 
 
 class maze{
 
 public:
 
 
-    /** checks if x,y position is a valid step 
+    /** checwks if x,y position is a valid step 
      * 
      * @param maze the 2D maze provided 
      * @param colSize x legnth of maze col 
@@ -19,14 +21,13 @@ public:
      * @return true if valid, else false 
      * 
      *  **/
-    bool isValidStep(std::vector<std::vector<int>> maze, int colSize, int rowSize, int curX, int curY){
+    bool isValidStep(const std::vector<std::vector<int>>& maze, int x, int y){
 
-        if(curX <= colSize && curY <= rowSize && maze[curX][curY] ==1){
+        if(x <= maze.size() && y <= maze[0].size() && maze[x][y] ==0){
                 return true; 
             }
-        return false; 
+        return false;
     }
-
     /** utility function that uses a recursive algorithm to traverse through "maze"
      * 
      * @param maze 2D matrix to represent maze consisting of ints as valid/non-valid states
@@ -34,54 +35,88 @@ public:
      * @param curX current x-position in maze
      * @param curY current y-position in maze
      * @return true if one of conditions are met
+     * 
      *  **/
     bool backtrack(std::vector<std::vector<int>> maze, std::vector<std::vector<int>> solPath, int curX, int curY){
 
         // check if current pos is end goal - making assumption we have beg/end
-        if ((curX == maze.size()-1) && maze[curX][curY] == 1){
-            solPath[curX][curY] = 1; 
+        if ((curX == maze.size()-1) && maze[curX][curY] == 0){
+            solPath[curX][curY] = 0; 
             return true; 
         }
         // check if x,y is valid 
-        if (isValidStep(maze, maze.size(), maze[0].size(), curX, curY)){
+        if (isValidStep(maze, curX, curY)){
             //ignore if we have already stored as solution path
             if (solPath[curX][curY] == 1){
                 return false;
             }
             //otherwise, add tile to solution path
-            solPath[curX][curY] = 1; 
-            //move forward
-            // use of recursive function
+            solPath[curX][curY] = 0; 
+            //check block right of current pos
             if (backtrack(maze, solPath, curX+1, curY) == true){
                 return true; 
             }
-            // if x isnt solution, go y direction 
+            // check block down of current pos
             if (backtrack(maze, solPath, curX, curY+1) == true){
                 return true; 
             }
+            // check block left of current pos
+            if (backtrack(maze, solPath, curX-1, curY+1) == true){
+                return true; 
+            }
             // else we have to backtrack and remove pos as part of path 
-            solPath[curX][curY] = 0;
+            solPath[curX][curY] = 1;
             return false;
         }
         return false;
     }
 
-    /** function that calls maze-solving utility function
-     * @param maze 2D matrix representing maze
-     * @param solPath 2D matrix for the solution path found
-     * @return true if sucessfully finds solution path
-     * 
-     * **/
-    bool solveMaze(std::vector<std::vector<int>> maze, std::vector<std::vector<int>> solPath){
-        if (backtrack(maze, solPath, 0, 0) == false){
-            return false;
-        }
-        return true; 
 
+    /** algorithm that uses a queue instead of recursion to find path through maze
+     * 
+     * @param maze 2D matrix representing our maze
+     * @param start starting x,y position 
+     * @param destination our exit or ending x,y position
+     * @return true if we we found valid path from start to destination
+     * 
+     *  **/
+    bool hasPath(std::vector<std::vector<int>>& maze, std::vector<int>& start, std::vector<int>& destination) {
+        int colSize = maze.size(), rowSize = maze[0].size();
+        int d[] = {0, 1, 0, -1, 0};
+        //queue to keep track of tiles to visit
+        std::queue<std::vector<int>> todo;
+        todo.push(start);
+        //set visited tiles as -1 
+        maze[start[0]][start[1]] = -1;
+
+        while (!todo.empty()) {
+            std::vector<int> p = todo.front();
+            todo.pop();
+            // check all four directions to see which way we can move
+            for (int i = 0; i < 4; i++) {
+                int col = p[0], row = p[1];
+                //check which side is valid
+                while (col + d[i] >= 0 && col + d[i] < colSize &&
+                         row + d[i + 1] >= 0 && row + d[i + 1] < rowSize && 
+                         maze[col + d[i]][row + d[i + 1]] <= 0) {
+
+                    col += d[i];
+                    row += d[i + 1];
+                }
+                //if we reached our target end tile, return
+                if (col == destination[0] && row == destination[1]) {
+                    return true;
+                }
+                if (!maze[col][row]) {
+                    maze[col][row] = -1;
+                    todo.push({col, row});
+                }
+            }
+        }
+        return false;
     }
 
-
-
+    
 };
 
 
--- myTests.cpp
+++ myTests.cpp
@@ -17,16 +17,15 @@
 TEST(storyOne, findTarget){
 
 maze solver; 
-std::string keyword = "empty";
 bool passing = false; 
 
 // Checking invalid tests
 {
-    std::vector<std::vector<int>> list = {{0, 0, 0, 0, 0, 0}};
+    std::vector<std::vector<int>> list = {{1, 1, 1, 1, 1, 1}};
     
     for (int i =0; i < list.size(); i++){
         for (int j=0; j < list[0].size(); j++){
-            if (solver.isValidStep(list, list.size(), list[0].size(), i, j)){
+            if (solver.isValidStep(list, i, j)){
             passing = true; 
             break; 
             }
@@ -38,11 +37,11 @@ bool passing = false;
 
 //valid test
 {
-    std::vector<std::vector<int>> list = {{0, 1, 0, 0, 0, 0}};
+    std::vector<std::vector<int>> list = {{1, 0, 1, 1, 1, 1}};
 
     for (int i =0; i < list.size(); i++){
         for (int j=0; j < list[0].size(); j++){
-            if (solver.isValidStep(list, list.size(), list[0].size(), i, j)){
+            if (solver.isValidStep(list, i, j)){
             passing = true; 
             break; 
             }
@@ -68,19 +67,18 @@ TEST(StoryTwo, walkHallway){
     maze solver;
     // example maze given - hard coding input may not be ideal but in this situation we are provided input
     std::vector<std::vector<int>> maze = { 
-        {0, 0, 1, 0},
-        {0, 0, 1, 0},
-        {0, 0, 1, 0},
-        {0, 0, 1, 0}
+        {1, 1, 0, 1},
+        {1, 1, 0, 1},
+        {1, 1, 0, 1},
+        {1, 1, 0, 1}
     };
     // solution matrix that will have the valid path included
     std::vector<std::vector<int>> solPath = { 
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0},
-        {0, 0, 0, 0}
+        {1, 1, 1, 1},
+        {1, 1, 1, 1},
+        {1, 1, 1, 1},
+        {1, 1, 1, 1}
     };
-
 
     // testing isValidStep() functionality 
     {
@@ -88,11 +86,10 @@ TEST(StoryTwo, walkHallway){
         int counter =0; 
 
         EXPECT_FALSE(pathSize == counter); 
-
         //nest for loop through all tiles in the maze and count number of "valid" tiles we expect
         for (int i=0; i<maze.size(); i++){
             for (int j=0; j<maze[i].size(); j++){
-                bool sol = solver.isValidStep(maze, maze.size(), maze[i].size(), i, j);
+                bool sol = solver.isValidStep(maze, i, j);
                 if (sol){
                     counter++;
                 }
@@ -101,17 +98,60 @@ TEST(StoryTwo, walkHallway){
         EXPECT_EQ(pathSize, counter);
     }
 
-    // testing backtracking, recursive algorithm to traverse maze and find path
-
+    // testing backtracking, recursive algorithm to traverse maze and find valid path
     {
         EXPECT_FALSE(maze == solPath);
-
         if(solver.backtrack(maze, solPath, 0, 0)){
             EXPECT_EQ(maze, solPath);
         }
+    }
+
+}
+
+/**
+ * ----------------------------------------------------------------
+ * User Story 3: 
+ * find way into and out of rooms
+ * 2D maze 
+ * looks like we may need a more effificent solver than previous recusion
+ * ----------------------------------------------------------------
+**/
+
+TEST(StoryThree, bfsMaze){
+
+    // testing parameters for story 3
+    maze solver;
+    // example maze given - hard coding input may not be ideal but in this situation we are provided input
+
+    // test bad start/dest input
+    {
+        std::vector<std::vector<int>> maze3 = { 
+        {1, 0, 1, 1, 1},
+        {1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 1},
+        {1, 1, 1, 0, 1}
+        };
+        // failing with invalid start, dest values
+        std::vector<int> start = {-2, 5};
+        std::vector<int> dest = {1, 5};
+        EXPECT_FALSE(solver.hasPath(maze3, start, dest));
+    }
+    // sucessfull 
+    {
+        std::vector<std::vector<int>> maze3 = { 
+        {1, 0, 1, 1, 1},
+        {1, 0, 0, 0, 1},
+        {1, 0, 0, 0, 1},
+        {1, 1, 1, 0, 1}
+        };
+        // sucessfull start and dest 
+        std::vector<int> start = {0,1};
+        std::vector<int> dest = {3,3};
+
+        EXPECT_TRUE(solver.hasPath(maze3, start, dest));
 
     }
 
 
-
 }
+
